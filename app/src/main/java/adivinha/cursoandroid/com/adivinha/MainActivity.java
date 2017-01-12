ed8fdca20cbe2939508cb7072a53b42195a896ab
salatielaizza <salatiel.aizza@gmail.com>
2017-01-12 00:14:21 +0000

    Android Adivinha

---
+++ app/src/main/java/adivinha/cursoandroid/com/adivinha/MainActivity.java
@@ -0,0 +1,37 @@
+package adivinha.cursoandroid.com.adivinha;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button botaoJogar;
+    private TextView resultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        botaoJogar = (Button) findViewById(R.id.botaoJogarId);
+        resultado = (TextView) findViewById(R.id.resultadoID);
+
+        //resultado.setText("Texto Alterado");
+
+        botaoJogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Random randomico = new Random();
+                int numeroAleatorio = randomico.nextInt(100);
+                resultado.setText("O número  escolhido é: " + numeroAleatorio);
+            }
+        });
+
+    }
+}
